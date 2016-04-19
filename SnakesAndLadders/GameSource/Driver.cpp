bf1141802b406176624e1e8bf46f3d4e39c891bc
Dominique Pennington <dompenn2010@gmail.com>
2016-04-18 21:38:11 -0400

    Ship

--- SnakesAndLadders/GameSource/Driver.cpp
+++ SnakesAndLadders/GameSource/Driver.cpp
@@ -7,16 +7,15 @@
 #include <sstream>
 using namespace std; // Shouldn't do this...
 
-
 ////////////////////////////////////////////
-class  Player{
+class  Player {
 
-public:	
+public:
 	Player()
 		: turn(1), location(0)
 	{}
 
-	void setLocation(int l){
+	void setLocation(int l) {
 		location = l;
 	}
 
@@ -24,18 +23,14 @@ public:
 		return location;
 	}
 
-	void nextTurn(int roll){
+	void nextTurn(int roll) {
 		turn++;
 		location += roll;
 	}
 
-	int getTurn(){
+	int getTurn() {
 		return turn;
 	}
-
-	//void playerRoll() {
-	//	location += roll();
-	//}
 
 private:
 	int location;
@@ -52,7 +47,7 @@ enum entityEnum {
 ////////////////////////////////////////////
 class Entity {
 
-public:	
+public:
 	Entity()
 	{}
 
@@ -60,7 +55,7 @@ public:
 		: magnitude(m), entityType(e)
 	{}
 
-	void setMagnitude(int m){
+	void setMagnitude(int m) {
 		magnitude = m;
 	}
 
@@ -82,46 +77,6 @@ private:
 };
 
 ///////////////////////////////////////////
-/*class Ladder: public Entity {
-
-public:
-	Ladder(int m)
-		:Entity(int m) 
-	{}
-};
-////////////////////////////////////////////
-class Snake: public Entity {
-
-public:
-
-	Snake(int m)
-		: magnitude(m) 
-	{}
-};*/
-///////////////////////////////////////////
-/*
-class Board{
-
-public:
-	Board(int w, int h)
-		: width(w), height(h) 
-	{}
-
-	int getSize() {
-		return boardSize;
-	}
-
-
-private:
-	int width;
-	int height;
-//	const int numOfEntities;
-
-	int boardSize = width*height;
-	
-};
-*/
-///////////////////////////////////////////
 bool readInput(int &boardSize, int &gamesToPlay, Entity gameBoard[]) {
 
 	string line;
@@ -129,7 +84,7 @@ bool readInput(int &boardSize, int &gamesToPlay, Entity gameBoard[]) {
 	if (myfile.is_open())
 	{
 		int inlineCount = 0;
-		int linecount = 0 ;
+		int linecount = 0;
 		int loc = 0;
 		int mag;
 		string type;
@@ -141,87 +96,47 @@ bool readInput(int &boardSize, int &gamesToPlay, Entity gameBoard[]) {
 
 			// First line = number of games to play
 			if (linecount == 1) {
-			
-				// So the "line" is counting 10 and 50 as one line - thats whats fucking me up
+					gamesToPlay = stoi(line);
+					cout << "One: " << line << "\n";
 
-				if (inlineCount == 0){
-				gamesToPlay = stoi(line);
-				cout << "One: " << line << "\n";
-				
-				linecount ++;
-				inlineCount++;
-			//	break;
-				}else if (inlineCount == 2){
-					
-							
+					linecount++;
+			}else if(linecount == 2) {
 				boardSize = stoi(line);
 				Entity *gameBoard = new Entity[boardSize];
-			
+
 				cout << "Two: " << line << "\n";
-				inlineCount = 0;
-		
+
+			}else {
+				if (iss >> line) {
+
+					switch (inlineCount) {
+					case 0: {
+						loc = stoi(line);
+						inlineCount++;
+						break;
+					}
+					case 1: {
+						type = line;
+						inlineCount++;
+						break;
+					}
+					default: {
+						mag = stoi(line);
+						gameBoard[loc].setMagnitude(mag);
+
+						if (type.compare("Snake")) {
+							gameBoard[loc].setType(Snake);
+						}
+						else if (type.compare("Ladder")) {
+							gameBoard[loc].setType(Ladder);
+						}
+						cout << "loc: " << loc << " " << "type " << gameBoard[loc].getType() << " " << "mag: " << gameBoard[loc].getMagnitude() << "\n";
+						inlineCount = 0;
+						break;
+					}
+					}
 				}
-					
 			}
-			
-			// Second = boardSize
-			/* else if(linecount == 2) {
-				boardSize = stoi(line);
-				Entity *gameBoard = new Entity[boardSize];
-			
-				cout << "Two: " << line << "\n";
-		
-				
-			}*/else{
-				if (iss >> line){	
-				
-					
-
-					switch (inlineCount){
-						case 0 : { 
-								loc = stoi(line);
-								inlineCount++;
-								break;
-							}
-						case 1 : {
-								type = line;
-								inlineCount++;
-								break;
-							}
-						default: {	
-								mag = stoi(line);
-								gameBoard[loc].setMagnitude(mag);
-	
-								if (type.compare("Snake")){
-									gameBoard[loc].setType(Snake);
-								}else if(type.compare("Ladder")){
-									gameBoard[loc].setType(Ladder);
-								}
-								cout << "loc: " << loc << " " << "type " << gameBoard[loc].getType() << " " << "mag: " << gameBoard[loc].getMagnitude() << "\n";								
-								inlineCount = 0;
-								break;
-							}
-					}		
-	
-
-
-
-				}
-		
-
-				/*
-				cout << "!!!!!!!!!" << (iss >> first_on_line);
-				gameBoard[first_on_line].setMagnitude(third_on_line);
-				gameBoard[first_on_line].setType(Ladder);
-
-				cout << "magnitude is: " << gameBoard[6].getMagnitude();
-				*/
-			}
-
-
-
-
-
 		}
 		myfile.close();
 		return true;
@@ -231,108 +146,63 @@ bool readInput(int &boardSize, int &gamesToPlay, Entity gameBoard[]) {
 	return false;
 }
 //////////////////////////////////////////
-
-void initBoard(int w, int h  /*, Entity[] e*/);
-void constructCreatures(int magnitude);
-//void constructBoard(int w, int h);
 int roll();
 bool battle(Player &p, Entity e, int boardSize);
 void playerStats(Player player);
 bool processTurn(Entity currentEntity, int boardSize, entityEnum entType);
-bool readInput(int &boardSize, int &gamesToPlay/*, Entity &gameBoard[]*/); 
+bool readInput(int &boardSize, int &gamesToPlay, Entity gameBoard[]);
 
 Player player;
 
 
 int main() {
-	
+
+	bool boardConstructed = false;
 	int gamesPlayed = 0;
 	int totalTurns = 0;
 	int gamesToPlay;
 	int boardSize;
-
-	Entity gameBoard [] = {};
-
-	//Entity gameBoard [] = 
+	Entity *gameBoard;
 
 	readInput(boardSize, gamesToPlay, gameBoard);
-	system("pause");
-	
-	
 
-	//Entity *gameBoard = new Entity[]; 
-	//Entity gameBoard[];
-/*
-	gameBoard[6].setMagnitude(8);
-	gameBoard[6].setType(Ladder);
 
-	gameBoard[13].setMagnitude(6);
-	gameBoard[13].setType(Snake);
+	for (int i = 0; i < gamesToPlay; i++) {
 
-	gameBoard[17].setMagnitude(8);
-	gameBoard[17].setType(Ladder);
-
-	gameBoard[21].setMagnitude(2);
-	gameBoard[21].setType(Snake);
-
-	gameBoard[29].setMagnitude(4);
-	gameBoard[29].setType(Ladder);
-
-	gameBoard[35].setMagnitude(8);
-	gameBoard[35].setType(Snake);
-
-	gameBoard[40].setMagnitude(5);
-	gameBoard[40].setType(Ladder);
-
-*/	
-	//int boardSize = (sizeof(gameBoard) / sizeof(*gameBoard));
-
-	for (int i = 0; i < gamesToPlay; i++){
-
-		bool playing = true;			
-		gamesPlayed ++;
+		bool playing = true;
+		gamesPlayed++;
 
 		// Game Loop
 		while (playing) {
 			Entity currentEntity = gameBoard[player.getLocation()];
 
 			playerStats(player);
-		
+
 			player.nextTurn(roll());
 
-
-
-			cout << "OH MY GOD!!!!!" << boardSize;
-		
-
-			// processTurn = true,
 			if (processTurn(currentEntity, boardSize, currentEntity.getType())) {
 				cout << "You Won in " << player.getTurn() << " turns!! Congratulations!\n\n";
-				
-
-
 				playing = false;
 				break;
 			}
-			
+
 		}
-	totalTurns += player.getTurn();
+		totalTurns += player.getTurn();
 	}
 
 	cout << "Total Turns: " << totalTurns << "\n";
-	cout << "Games played: " << gamesPlayed << "\nAverage turns per game: " << ((double)totalTurns/(double)gamesPlayed) << "\n";
+	cout << "Games played: " << gamesPlayed << "\nAverage turns per game: " << ((double)totalTurns / (double)gamesPlayed) << "\n";
+
 	system("pause");
 	return 0;
 }
 
 bool processTurn(Entity currentEntity, int boardSize, entityEnum entType) {
-	//if (entType != NULL) {
 
-		if (battle(player, currentEntity, boardSize)) {
-			return true;
-		}
-	//}
-	
+	if (battle(player, currentEntity, boardSize)) {
+		return true;
+	}
+
 	return false;
 }
 
@@ -343,73 +213,29 @@ void playerStats(Player player) {
 
 }
 
-/*
-void constructBoard(int w, int h) {
-	Board gameBoard(w, h);
-	cout << "The board is: " << gameBoard.getSize() << "\n";
-
-
-//	int input;
-//	cin >> input; 
-	
-}
-*/
-
-void constructCreatures(int magnitude) {
-/*
-	Snake snake1(magnitude);
-	cout << "The snake is: " << snake1.getMagnitude() << "\n";
-
-	Ladder ladder1(magnitude);
-	cout << "The ladder is: " << ladder1.getMagnitude() << "\n";
-*/	
-// Implement this later
-//	Entity gameBoard[*a];
-
-//	for (int i = 0; i < boardSize - 1; i++) {
-
-//	}
-
-//	return gameBoard;
-}
-
-
-/*Use this to initialize the game - should only take in the board size and location of entities */
-/*
-void initBoard(int w, int h ) {
-
-	constructBoard(w, h);
-	//constructCreatures(e);
-
-}
-*/
-int roll(){
+int roll() {
 
 	int roll;
-	srand (time(NULL) + clock()); // It just didn't look random enough with one or the other so I used both?
+	srand(time(NULL) + clock()); // It just didn't look random enough with one or the other so I used both?
 
 	roll = rand() % 6 + 1;
-	
+
 	cout << "You rolled a " << roll << "!\n";
 	return roll;
 }
 
 // In the event that a player reaches an entity, adjust location based on entity type
-bool battle(Player &p, Entity e, int boardSize){
+bool battle(Player &p, Entity e, int boardSize) {
 	int initialLoc = p.getLocation();
 	int entityMag = e.getMagnitude();
 	entityEnum entityType = e.getType();
-	bool didJaWin = false; 
-	cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << "\n";
+	bool didJaWin = false;
 
-	// Fix this... Calling this twice is stupid.	
 	// If player reaches end of board, win
 	if (p.getLocation() >= boardSize) {
 		didJaWin = true;
 		return didJaWin;
 	}
-
-	cout << "Made it." << boardSize << "\n"; 
 	// Players location = current location + magnitude of entity
 	if (entityType == Snake) {
 		cout << "You hit a snake at location: " << initialLoc << " with a magnitude of " << entityMag << "\n";
@@ -418,17 +244,12 @@ bool battle(Player &p, Entity e, int boardSize){
 	if (entityType == Ladder) {
 		cout << "You a found a ladder at location: " << initialLoc << " with a magnitude of " << entityMag << "\n";
 		p.setLocation(p.getLocation() + entityMag);
-
-	}if (entityType == Vacant) {
-		cout << "NOTHINGS HERE, FREDDIE!!!";
 	}
-
 
 	// If snake sends player below location 0, fix it
 	if (p.getLocation() < 0) {
 		p.setLocation(0);
 	}
-
 
 	// If player reaches end of board, win
 	if (p.getLocation() >= boardSize) {
@@ -436,6 +257,5 @@ bool battle(Player &p, Entity e, int boardSize){
 		return didJaWin;
 	}
 
-
 	return didJaWin;
-}	
+}
